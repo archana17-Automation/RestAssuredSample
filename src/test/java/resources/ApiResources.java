ba99edd337c6451340cebffbe0af7d5ba50dbd1d
Archana <singh.archana170489@gmail.com>
2021-06-21 12:13:37 +0530

    first commit

---
+++ src/test/java/resources/ApiResources.java
@@ -0,0 +1,21 @@
+package resources;
+//enum is special class in java which has collection of constants or  methods
+public enum ApiResources {
+	
+	AddPlaceAPI("/maps/api/place/add/json"),
+	getPlaceAPI("/maps/api/place/get/json"),
+	deletePlaceAPI("/maps/api/place/delete/json");
+	private String resource;
+	
+	ApiResources(String resource)
+	{
+		this.resource=resource;
+	}
+	
+	public String getResource()
+	{
+		return resource;
+	}
+	
+
+}
